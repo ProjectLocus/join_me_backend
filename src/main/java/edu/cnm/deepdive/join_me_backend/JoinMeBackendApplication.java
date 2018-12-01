de5e6d0cdd29e99740a473a09b9efc8886c1f5a1
bBleck <bmbleck@hotmail.com>
2018-12-01 09:50:22 -0700

    First commit to public directory, does not run currently.

--- src/main/java/edu/cnm/deepdive/join_me_backend/JoinMeBackendApplication.java
+++ src/main/java/edu/cnm/deepdive/join_me_backend/JoinMeBackendApplication.java
@@ -2,8 +2,12 @@ package edu.cnm.deepdive.join_me_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableEntityLinks;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableEntityLinks
 @SpringBootApplication
+@EnableScheduling
 public class JoinMeBackendApplication {
 
   public static void main(String[] args) {
