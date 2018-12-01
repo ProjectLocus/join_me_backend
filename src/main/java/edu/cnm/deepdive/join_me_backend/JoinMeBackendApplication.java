5784758ebf77510432626ba07d04bf3e9b200ead
bBleck <bmbleck@hotmail.com>
2018-12-01 06:25:51 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/join_me_backend/JoinMeBackendApplication.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.join_me_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinMeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JoinMeBackendApplication.class, args);
+  }
+}
