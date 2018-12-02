84f18e0c2ac97240fba11e630225544a04cc31d2
bBleck <bmbleck@hotmail.com>
2018-12-02 16:10:55 -0700

    Added mappings to get single invitation and all invitations.

--- src/main/java/edu/cnm/deepdive/join_me_backend/controller/InvitationController.java
+++ src/main/java/edu/cnm/deepdive/join_me_backend/controller/InvitationController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +42,11 @@ public class InvitationController {
   public List<Invitation> list() {
     return invitationRepository.findAllByOrderByIdAsc();
   }
+
+  @GetMapping(value = "{invitationId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Invitation getSingleInvitation(@PathVariable ("invitationId") int invitationId){
+    return invitationRepository.findById(invitationId).get();
+  }
+
+
 }
